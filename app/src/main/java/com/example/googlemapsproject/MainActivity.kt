0643c1da8976db2896b94e1f7b557472ec0769f2
lupahaosit <igor2002vlad@mail.ru>
2025-03-01 14:55:32 +0500

    geolocation getted

--- app/src/main/java/com/example/googlemapsproject/MainActivity.kt
+++ app/src/main/java/com/example/googlemapsproject/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.googlemapsproject
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,31 +14,80 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentActivity
 import com.example.googlemapsproject.ui.theme.GoogleMapsProjectTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : FragmentActivity(), OnMapReadyCallback {
-    lateinit var mMap:GoogleMap
+class MainActivity : FragmentActivity(), OnMapReadyCallback , GoogleMap.OnMarkerClickListener {
+    lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    companion object {
+        private const val LOCATION_PERMISSION_CODE = 1
+    }
+
+    private lateinit var lastLocation: Location
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         setContentView(R.layout.activity_main)
-        var mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
 
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        var point  = LatLng(57.17595798972696, 65.56815131754117)
+        map = googleMap
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnMarkerClickListener(this)
 
-        mMap.addMarker(MarkerOptions().position(point).title("Перекресток"))
-        mMap.isBuildingsEnabled = true
-        mMap.isIndoorEnabled = true
+        // Проверка разрешений
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_CODE
+            )
+            return
+        }
+
+        map.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+    }
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+        return false
+    }
+
+    // Обрабатываем результат запроса разрешений
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    map.isMyLocationEnabled = true
+                }
+            } else {
+                Toast.makeText(this, "Разрешение на геолокацию не получено", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
 
